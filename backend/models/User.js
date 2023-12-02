45ea31c20af03640a53a5c036141884c50b24917
rishibakshi <guitarguyrishi@gmail.com>
2023-12-03 01:12:24 +0530

    feat(model): add User model
    - Define a Mongoose schema for the User entity
    - Include fields for name, email, password, and isVerified
    - Configure 'name' as a required field
    - Ensure 'email' is unique and required
    - Specify 'password' as a required field
    - Introduce 'isVerified' with a default value of 'false' for email verification

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+const {Schema}=mongoose
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+})
+
+module.exports=mongoose.model("User",userSchema)
