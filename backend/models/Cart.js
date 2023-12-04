780ee39c008eed14e30f96181920ad651ac62475
rishibakshi <guitarguyrishi@gmail.com>
2023-12-04 19:20:42 +0530

    feat(model): add cart model
    - Introduce the 'Cart' model schema to manage user shopping carts.
    - The schema includes a reference to the 'User' model, ensuring association with specific users.
    - Each cart contains an array of items, defined by the 'itemSchema'.
    - The 'itemSchema' includes a reference to the 'Product' model and a quantity field.
    - This model structure supports the addition of products to the cart along with their respective quantities.
    - The schema excludes the version key for simplicity and clarity.
    - The implementation aligns with conventional practices for modeling shopping carts in e-commerce applications.

---
+++ backend/models/Cart.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose")
+const {Schema}=mongoose
+
+const itemSchema=new Schema({
+    product:{
+        type:Schema.Types.ObjectId,
+        ref:"Product",
+        required:true
+    },
+    quantity:{
+        type:Number,
+        required:true,
+        default:1
+    }
+})
+
+const cartSchema=new Schema({
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    items:{
+        type:[itemSchema],
+        required:true
+    }
+},{versionKey:false})
+
+module.exports=mongoose.model("Cart",cartSchema)
