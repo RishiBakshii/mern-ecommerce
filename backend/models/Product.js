d92373e3db2605dc1281d57241e410837c8261fd
rishibakshi <guitarguyrishi@gmail.com>
2023-12-04 18:51:39 +0530

    feat(model): add product model
    - Implement a robust product schema.
    - The schema includes essential fields such as title, description, price, discountPercentage, category, brand, stockQuantity, images, and isDeleted.
    - Default values are set for discountPercentage and isDeleted, following best practices.
    - Timestamps are integrated to track document creation and modification.
    - This schema provides a solid foundation for managing product data in the MongoDB collection.

---
+++ backend/models/Product.js
@@ -0,0 +1,43 @@
+const mongoose=require("mongoose")
+const {Schema}=mongoose
+
+const productSchema= new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    discountPercentage: {
+        type: Number,
+        default: 0,
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    brand:{
+        type:String,
+        required:true
+    },
+    stockQuantity:{
+        type:Number,
+        required:true
+    },
+    images:{
+        type:[String],
+        required:true
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true,versionKey:false})
+
+module.exports=mongoose.model('Product',productSchema)
