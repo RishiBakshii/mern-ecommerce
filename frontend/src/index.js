b7b8e73970c5e39bcbe487c77a86e678c4829c7b
rishibakshi <guitarguyrishi@gmail.com>
2023-12-08 19:54:20 +0530

    feat(ReduxToolkit): Set up Redux Store with Provider
    - Create 'src/app/store.js' for configuring the Redux store using @reduxjs/toolkit.
    - Integrate the Redux store with the React application using the <Provider> component in 'src/index.js'.
    - Prepare the foundation for managing global state with Redux.
    
    This commit establishes the basic setup for Redux integration in the project.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Provider} from 'react-redux'
+import { store } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
