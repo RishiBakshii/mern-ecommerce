7a8198f5041a1bca6e72cbef2a2c703ef6633c87
rishibakshi <guitarguyrishi@gmail.com>
2023-12-09 19:45:38 +0530

    feat(reduxToolkit-auth): Create Protected component
    - This component take the page as children prop, if the user is loggedIn then returns the children prop, Otherwise return a redirect to /login
    
    This commit will enhance the security of the website by making the routes protected only for loggedIn users

---
+++ frontend/src/features/auth/components/Protected.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux"
+import { selectLoggedInUser } from "../AuthSlice"
+import { Navigate } from "react-router"
+
+
+export const Protected = ({children}) => {
+    const loggedInUser=useSelector(selectLoggedInUser)
+
+    if(loggedInUser){
+        return children
+    }
+    return <Navigate to={'/login'} replace={true}/>
+}
