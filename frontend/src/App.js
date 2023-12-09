33eee1e4ffed8460a6fd228dbd0a0172b830fee9
rishibakshi <guitarguyrishi@gmail.com>
2023-12-09 15:30:17 +0530

    feat: Configure React Router with initial route structure
    - Add 'BrowserRouter' from 'react-router-dom'
    - Create basic route structure inside the 'App' component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import {BrowserRouter as Router,Routes,Route} from  'react-router-dom'
+
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+      </Routes>
+    </Router>
   );
 }
 
