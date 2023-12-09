feff88d2a0c67d3777834cca3c93eafcea63c4d5
rishibakshi <guitarguyrishi@gmail.com>
2023-12-09 15:20:49 +0530

    feat: Implement MUI theme and provider setup
    - Create 'theme.js' file with MUI theme configuration at 'src/theme/theme.js'
    - Set up MUI ThemeProvider in 'index.js' to apply the theme to the entire app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,18 @@ import {Provider} from 'react-redux'
 import { store } from './app/store';
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-      <ToastContainer position='top-right'/>
-    </Provider>
+    <ThemeProvider theme={theme}>
+        <Provider store={store}>
+              <App />
+              <ToastContainer position='top-right'/>
+        </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
