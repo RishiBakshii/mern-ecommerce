ae5940103c96252798e40c931410acef2ad3e844
rishibakshi <guitarguyrishi@gmail.com>
2023-12-09 13:16:10 +0530

    chore: install and set up 'react-toastify'
    - Add 'react-toastify' to the project dependencies
    - Configure 'react-toastify' in the project
    - Utilize 'react-toastify' for displaying toast notifications

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from 'react-redux'
 import { store } from './app/store';
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer position='top-right'/>
     </Provider>
   </React.StrictMode>
 );
