917f04f96bcfc2bfe6f521101743bc4d6fb9603e
rishibakshi <guitarguyrishi@gmail.com>
2023-12-10 17:20:26 +0530

    feat(reduxToolkit-products): Add API functions for products feature
    - Implement functions in 'ProductApi.js' for product-related API calls

---
+++ frontend/src/features/products/ProductApi.jsx
@@ -0,0 +1,50 @@
+import { axiosi } from "../../config/axios";
+
+export const addProduct=async(data)=>{
+    try {
+        const res=await axiosi.post('/products',data)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const fetchProducts=async()=>{
+    try {
+        const res=await axiosi.get('/products')
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const fetchProductById=async(id)=>{
+    try {
+        const res=await axiosi.get(`/products/${id}`)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const updateProductById=async(update)=>{
+    try {
+        const res=await axiosi.patch(`/products/${update._id}`,update)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const undeleteProductById=async(id)=>{
+    try {
+        const res=await axiosi.patch(`/products/${id}`)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const deleteProductById=async(id)=>{
+    try {
+        const res=await axiosi.delete(`/products/${id}`)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
