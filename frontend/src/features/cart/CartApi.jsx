fffd9dabb440d94df81bbb81252aa16711601a8d
rishibakshi <guitarguyrishi@gmail.com>
2023-12-14 18:21:02 +0530

    feat(reduxToolkit-cart): Add API functions for cart feature
    - Implement functions in 'CartApi.jsx' for cart-related API calls

---
+++ frontend/src/features/cart/CartApi.jsx
@@ -0,0 +1,34 @@
+import {axiosi} from '../../config/axios'
+
+export const addToCart=async(item)=>{
+    try {
+        const res=await axiosi.post('/cart',item)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const fetchCartByUserId=async(id)=>{
+    try {
+        const res=await axiosi.get(`/cart/user/${id}`)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const updateCartItemById=async(update)=>{
+    try {
+        const res=await axiosi.patch(`/cart/${update._id}`,update)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const deleteCartItemById=async(id)=>{
+    try {
+        const res=await axiosi.delete(`/cart/${id}`)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
