c57733608fa77941b5a4f8f44644324eba3285a4
rishibakshi <guitarguyrishi@gmail.com>
2023-12-16 10:34:42 +0530

    refactor(Model): Separate 'Address' schema into dedicated collection
    - Refactor the User model to separate the Address schema into a dedicated collection named "Address"
    - This improve data organization and simplifies query operations for both User and Address data
    - Previously, addresses were embedded within the User document, leading to potential complexity and redundancy
    
    This commit promotes better data isolation and scalability, allowing for independent management and manipulation of User and Address information

---
+++ backend/models/Address.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose')
+const {Schema}=mongoose
+
+const addressSchema = new Schema({
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  });
+
+module.exports=mongoose.model('Address',addressSchema)
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,34 +1,6 @@
 const mongoose=require("mongoose")
 const {Schema}=mongoose
 
-
-const addressSchema = new Schema({
-    street: {
-      type: String,
-      required: true,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    state: {
-      type: String,
-      required: true,
-    },
-    postalCode: {
-      type: String,
-      required: true,
-    },
-    country: {
-      type: String,
-      required: true,
-    },
-    type: {
-      type: String,
-      required: true,
-    },
-  });
-
 const userSchema=new Schema({
     name:{
         type:String,
@@ -42,10 +14,6 @@ const userSchema=new Schema({
     password:{
         type:String,
         required:true
-    },
-    addresses:{
-        type:[addressSchema],
-        
     },
     isVerified:{
         type:Boolean,
