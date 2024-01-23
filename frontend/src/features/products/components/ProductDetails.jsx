2bd870a337c1d6e5dcd9020704249c65a5412fcf
rishibakshi <guitarguyrishi@gmail.com>
2024-01-23 13:38:00 +0530

    feat(ProductDetails): Revamp UI & user actions for enhanced experience
    - Rebuilt component ui from scratch for better styling and maintanibality.
    - Allow user to select desired quantity before adding to cart, promoting flexibility and control.
    - Enable direct product addition to wishlist from the product details page.
    - Implement size and color selection for products.

--- frontend/src/features/products/components/ProductDetails.jsx
+++ frontend/src/features/products/components/ProductDetails.jsx
@@ -2,35 +2,44 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { fetchProductByIdAsync, selectSelectedProduct } from '../ProductSlice'
-import { Alert,Button,Rating, Stack, TextField, Typography, useTheme } from '@mui/material'
+import { Box,Checkbox,Radio,Rating, Stack,Typography } from '@mui/material'
 import { addToCartAsync, resetCartItemAddStatus, selectCartItemAddStatus, selectCartItems } from '../../cart/CartSlice'
 import { selectLoggedInUser } from '../../auth/AuthSlice'
-import { createReviewAsync, fetchReviewsByProductIdAsync, selectReviewStatus,} from '../../review/ReviewSlice'
-import { useForm } from "react-hook-form"
-import {LoadingButton} from '@mui/lab'
+import { fetchReviewsByProductIdAsync,selectReviews,} from '../../review/ReviewSlice'
 import { Reviews } from '../../review/components/Reviews'
 import {toast} from 'react-toastify'
+import {MotionConfig, motion} from 'framer-motion'
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import CachedOutlinedIcon from '@mui/icons-material/CachedOutlined';
+import Favorite from '@mui/icons-material/Favorite'
+import { createWishlistItemAsync, deleteWishlistItemByIdAsync, resetWishlistItemAddStatus, resetWishlistItemDeleteStatus, selectWishlistItemAddStatus, selectWishlistItemDeleteStatus, selectWishlistItems } from '../../wishlist/WishlistSlice'
 
+const SIZES=['XS','S','M','L','XL']
 
 export const ProductDetails = () => {
     const {id}=useParams()
-    const [value, setValue] = useState(1);
     const product=useSelector(selectSelectedProduct)
     const loggedInUser=useSelector(selectLoggedInUser)
     const dispatch=useDispatch()
     const cartItems=useSelector(selectCartItems)
-    const theme=useTheme()
-    const reviewStatus=useSelector(selectReviewStatus)
     const cartItemAddStatus=useSelector(selectCartItemAddStatus)
-    
+    const [quantity,setQuantity]=useState(1)
+    const [selectedSize,setSelectedSize]=useState('')
+    const reviews=useSelector(selectReviews)
+    const [selectedImageIndex,setSelectedImageIndex]=useState(0)
+
+    const wishlistItems=useSelector(selectWishlistItems)
 
     const isProductAlreadyInCart=cartItems.some((item)=>item.product._id===id)
+    const isProductAlreadyinWishlist=wishlistItems.some((item)=>item.product._id===id)
 
+    const totalReviewRating=reviews.reduce((acc,review)=>acc+review.rating,0)
+    const totalReviews=reviews.length
+    const averageRating=parseInt(Math.ceil(totalReviewRating/totalReviews))
 
-    const handleAddToCart=()=>{
-        const item={user:loggedInUser._id,product:id,quantity:1}
-        dispatch(addToCartAsync(item))
-    }
+    const wishlistItemAddStatus=useSelector(selectWishlistItemAddStatus)
+    const wishlistItemDeleteStatus=useSelector(selectWishlistItemDeleteStatus)
 
     
     useEffect(()=>{
@@ -56,73 +65,217 @@ export const ProductDetails = () => {
         }
 
     },[cartItemAddStatus])
+
+    useEffect(()=>{
+        if(wishlistItemAddStatus==='fulfilled'){
+            toast.success("Product added to wishlist")
+        }
+        else if(wishlistItemAddStatus==='rejected'){
+            toast.error("Error adding product to wishlist, please try again later")
+        }
+
+        return ()=>{
+            dispatch(resetWishlistItemAddStatus())
+        }
+
+    },[wishlistItemAddStatus])
+
+    useEffect(()=>{
+        if(wishlistItemDeleteStatus==='fulfilled'){
+            toast.success("Product removed from wishlist")
+        }
+        else if(wishlistItemDeleteStatus==='rejected'){
+            toast.error("Error removing product from wishlist, please try again later")
+        }
+
+        return ()=>{
+            dispatch(resetWishlistItemDeleteStatus())
+        }
+    },[wishlistItemDeleteStatus])
+
+    const handleAddToCart=()=>{
+        const item={user:loggedInUser._id,product:id,quantity}
+        dispatch(addToCartAsync(item))
+        setQuantity(1)
+    }
+
+    const handleDecreaseQty=()=>{
+        if(quantity!==1){
+            setQuantity(quantity-1)
+        }
+    }
+    
+    const handleIncreaseQty=()=>{
+        if(quantity<20 && quantity<product.stock){
+            setQuantity(quantity+1)
+        }
+    }
+
+    const handleSizeSelect=(size)=>{
+        setSelectedSize(size)
+    }
+
+    const handleAddRemoveFromWishlist=(e)=>{
+        if(e.target.checked){
+            const data={user:loggedInUser?._id,product:id}
+            dispatch(createWishlistItemAsync(data))
+        }
+
+        else if(!e.target.checked){
+            const index=wishlistItems.findIndex((item)=>item.product._id===id)
+            dispatch(deleteWishlistItemByIdAsync(wishlistItems[index]._id));
+        }
+    }
     
 
-
   return (
-    <Stack alignItems={'center'}>
+    <Stack sx={{justifyContent:'center',alignItems:'center'}}>
 
-        {/* parent */}
-        <Stack width={'80%'} rowGap={5} justifyContent={'center'} mb={5}>
+        <Stack width={'88rem'} height={"50rem"} rowGap={5} mt={5} justifyContent={'center'} mb={5} flexDirection={"row"} columnGap={"5rem"}>
 
-            {/* upper */}
-            <Stack mt={5} gap={2} height={"30rem"}  flexDirection={'row'} justifyContent={'space-evenly'}>
+            {/* left stack (images) */}
+            <Stack  sx={{flexDirection:"row",columnGap:"2.5rem",alignSelf:"flex-start",height:"100%"}}>
 
-                <Stack height={'100%'}>
-                    <img style={{width:'100%',height:"100%",objectFit:"contain"}} src={product?.images[0]} alt="" />
+                {/* image selection */}
+                <Stack sx={{display:"flex",rowGap:'1.5rem',height:"100%",overflowY:"scroll"}}>
+                    {
+                        product && product.images.map((image,index)=>(
+                            <motion.div  whileHover={{scale:1.1}} whileTap={{scale:1}} style={{width:"200px",cursor:"pointer"}} onClick={()=>setSelectedImageIndex(index)}>
+                                <img style={{width:"100%",objectFit:"contain"}} src={image} alt={`${product.title} image`} />
+                            </motion.div>
+                        ))
+                    }
+                </Stack>
+                
+                {/* selected image */}
+                <Stack sx={{alignSelf:"center"}}>
+                    <div style={{width:"100%"}}>
+                        <img style={{width:"100%",objectFit:"contain"}} src={product?.images[selectedImageIndex]} alt={`${product?.title} image`} />
+                    </div>
                 </Stack>
 
-                <Stack >
-                    <Stack height={'50%'}>
-                        <img style={{width:'100%',height:"100%",objectFit:"contain"}} src={product?.images[1]} alt="" />
-                    </Stack>
-                    <Stack height={'50%'}>
-                        <img style={{width:'100%',height:"100%",objectFit:"contain"}} src={product?.images[2]} alt="" />
-                    </Stack>
-                </Stack>
-
-                <Stack height={'100%'}>
-                    <img style={{width:'100%',height:"100%",objectFit:"contain"}} src={product?.images[3]} alt="" />
-                </Stack>
             </Stack>
 
-            {/* lower */}
-            <Stack flex={1} flexDirection={'row'} columnGap={2} flexWrap={'wrap'}>
-                <Stack flex={4} rowGap={2}>
-                    <Stack>
-                        <Typography variant='h4'>{product?.title}</Typography>
-                        <Typography variant='body2' color={'text.secondary'}>{product?.brand.name}</Typography>
+            {/* right stack - about product */}
+            <Stack rowGap={"1.5rem"} width={'25rem'}>
+
+                {/* title rating price */}
+                <Stack rowGap={".5rem"}>
+
+                    {/* title */}
+                    <Typography variant='h4' fontWeight={600}>{product?.title}</Typography>
+
+                    {/* rating */}
+                    <Stack sx={{flexDirection:"row",columnGap:"1rem",alignItems:"center"}}>
+                        <Rating value={averageRating} readOnly/>
+                        <Typography>( {totalReviews===0?"No reviews":totalReviews===1?`${totalReviews} Review`:`${totalReviews} Reviews`} )</Typography>
+                        <div style={{backgroundColor:"green",height:"100%",width:"1px"}}></div>
+                        <Typography color={product?.stock<=10?"error":product?.stock<=20?"orange":"green"}>{product?.stock<=10?`Only ${product?.stock} left`:product?.stock<=20?"Only few left":"In Stock"}</Typography>
                     </Stack>
-                    <Typography variant='h6' fontWeight={400}>{product?.description}</Typography>
+
+                    {/* price */}
+                    <Typography variant='h5'>${product?.price}</Typography>
                 </Stack>
 
-                <Stack flex={2} justifyContent={'space-between'}>
-                        <Typography variant='h5'>${product?.price}</Typography>
+                {/* description */}
+                <Stack rowGap={".8rem"}>
+                    <Typography>{product?.description}</Typography>
+                    <hr />
+                </Stack>
+
+                {/* color, size and add-to-cart */}
+                <Stack sx={{rowGap:"1.3rem"}}>
+
+                    {/* colors */}
+                    <Stack flexDirection={'row'} alignItems={'center'} columnGap={'.2rem'}>
+                        <Typography>Colors: </Typography>
+                        <Stack flexDirection={'row'}>
+                            <Radio/>
+                            <Radio/>
+                        </Stack>
+                    </Stack>
+                    
+                    {/* size */}
+                    <Stack flexDirection={'row'} alignItems={'center'} columnGap={'1rem'}>
+                        <Typography>Size: </Typography>
+                        <Stack flexDirection={'row'} columnGap={'1rem'}>
+                            {
+                                SIZES.map((size)=>(
+                                    <motion.div onClick={()=>handleSizeSelect(size)} whileHover={{scale:1.050}} whileTap={{scale:1}} style={{border:selectedSize===size?'':"1px solid grayText",borderRadius:"8px",width:"30px",height:"30px",display:"flex",justifyContent:"center",alignItems:"center",cursor:"pointer",padding:"1.2rem",backgroundColor:selectedSize===size?"#DB4444":"whitesmoke",color:selectedSize===size?"white":""}}>
+                                        <p>{size}</p>
+                                    </motion.div>
+                                ))
+                            }
+                        </Stack>
+                    </Stack>
+
+                    {/* quantity , add to cart and wishlist */}
+                    <Stack flexDirection={"row"} columnGap={"1.5rem"} width={'100%'} >
+                        
+                        {/* qunatity */}
+                        <Stack flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'}>
+                            
+                            <MotionConfig whileHover={{scale:1.050}} whileTap={{scale:1}}>
+                                <motion.button onClick={handleDecreaseQty}  style={{padding:"10px 15px",fontSize:"1.050rem",backgroundColor:"",color:"black",outline:"none",border:'1px solid black',borderRadius:"8px"}}>-</motion.button>
+                                <p style={{margin:"0 1rem",fontSize:"1.1rem",fontWeight:'400'}}>{quantity}</p>
+                                <motion.button onClick={handleIncreaseQty} style={{padding:"10px 15px",fontSize:"1.050rem",backgroundColor:"black",color:"white",outline:"none",border:'none',borderRadius:"8px"}}>+</motion.button>
+                            </MotionConfig>
+
+                        </Stack>
+                        
+                        {/* add to cart */}
                         {
-                            isProductAlreadyInCart?(
-                                <Alert severity='info' color={'info'} sx={{bgcolor:theme.palette.primary.light}}>This Product is Already in your cart</Alert>
-                            ):(
-
-                                loggedInUser?.isAdmin?null:
-                                <Button variant='contained' onClick={handleAddToCart}>Add to cart</Button>
-                            )
+                            isProductAlreadyInCart?
+                            <button style={{padding:"10px 15px",fontSize:"1.050rem",backgroundColor:"black",color:"white",outline:"none",border:'none',borderRadius:"8px"}}>Already in Cart</button>
+                            :<motion.button whileHover={{scale:1.050}} whileTap={{scale:1}} onClick={handleAddToCart} style={{padding:"10px 15px",fontSize:"1.050rem",backgroundColor:"black",color:"white",outline:"none",border:'none',borderRadius:"8px"}}>Add To Cart</motion.button>
                         }
+
+                        {/* wishlist */}
+                        <motion.div style={{border:"1px solid grayText",borderRadius:"4px",display:"flex",justifyContent:"center",alignItems:"center"}}>
+                            <Checkbox checked={isProductAlreadyinWishlist} onChange={(e)=>handleAddRemoveFromWishlist(e)} icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{color:'red'}} />} />
+                        </motion.div>
+
+
+                    </Stack>
+
                 </Stack>
+
+
+                {/* product perks */}
+                <Stack mt={3} sx={{justifyContent:"center",alignItems:'center',border:"1px grayText solid",borderRadius:"7px"}}>
+                    
+                    <Stack p={2} flexDirection={'row'} alignItems={"center"} columnGap={'1rem'} width={'100%'} justifyContent={'flex-sart'}>
+                        <Box>
+                            <LocalShippingOutlinedIcon/>
+                        </Box>
+                        <Stack>
+                            <Typography>Free Delivery</Typography>
+                            <Typography>Enter your postal for delivery availabity</Typography>
+                        </Stack>
+                    </Stack>
+                    <hr style={{width:"100%"}} />
+                    <Stack p={2} flexDirection={'row'} alignItems={"center"} width={'100%'} columnGap={'1rem'} justifyContent={'flex-start'}>
+                        <Box>
+                            <CachedOutlinedIcon/>
+                        </Box>
+                        <Stack>
+                            <Typography>Return Delivery</Typography>
+                            <Typography>Free 30 Days Delivery Returns</Typography>
+                        </Stack>
+                    </Stack>
+
+                </Stack>
+
             </Stack>
-
-
-            {/* reviews */}
-            <Stack mt={5}  alignSelf={'flex-start'}>
-
-                <Reviews productId={id}/>       
-
-            </Stack>
-
-
+            
         </Stack>
 
-
-
+        {/* reviews */}
+        <Stack width={'88rem'}>
+            <Reviews productId={id} averageRating={averageRating}/>       
+        </Stack>
+                
     </Stack>
+
   )
 }
