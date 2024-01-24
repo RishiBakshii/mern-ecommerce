d31802fcbd08fab707bd3837fc438a81dba8da4a
rishibakshi <guitarguyrishi@gmail.com>
2024-01-24 17:08:02 +0530

    feat(Footer): Add Footer component - Create Footer.jsx at 'src/features/footer/Footer.jsx' - Attach Footer in 'ProductDetailsPage' - Add appStore.png, facebook.png, googlePlay.png, instagram.png, linkedin.png, QRCode.png and twitter.png at 'src/assets/images', require in 'Footer' component

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -5,6 +5,13 @@ import noOrdersAnimation from './animations/noOrders.json'
 import emptyWishlistAnimation from './animations/emptyWishlist.json'
 import notFoundPageAnimation from './animations/notFoundPage.json'
 import banner1 from './images/banner1.jpg'
+import googlePlayPng from './images/googlePlay.png'
+import appStorePng from './images/appStore.png'
+import  QRCodePng from './images/QRCode.png'
+import facebookPng from './images/facebook.png'
+import twitterPng from './images/twitter.png'
+import instagramPng from './images/instagram.png'
+import linkedinPng from './images/linkedin.png'
 
 export {
     ecommerceOutlookAnimation,
@@ -14,4 +21,11 @@ export {
     emptyWishlistAnimation,
     notFoundPageAnimation,
     banner1,
+    googlePlayPng,
+    appStorePng,
+    QRCodePng,
+    facebookPng,
+    twitterPng,
+    instagramPng,
+    linkedinPng
 }
