afc5cfae0bbbbff75d5c952b744706960f0f91ad
rishibakshi <guitarguyrishi@gmail.com>
2024-01-26 18:46:31 +0530

    fix(Footer): Implement responsive layout for optimal viewing across devices

--- frontend/src/features/footer/Footer.jsx
+++ frontend/src/features/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, TextField, Typography, useTheme } from '@mui/material'
+import { Box, IconButton, TextField, Typography, useMediaQuery, useTheme } from '@mui/material'
 import { Stack } from '@mui/material'
 import React from 'react'
 import { QRCodePng, appStorePng, googlePlayPng ,facebookPng,instagramPng,twitterPng,linkedinPng} from '../../assets'
@@ -11,6 +11,7 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
 
     const theme=useTheme()
+    const is700=useMediaQuery(theme.breakpoints.down(700))
 
     const labelStyles={
         fontWeight:300,
@@ -18,26 +19,26 @@ export const Footer = () => {
     }
 
   return (
-    <Stack sx={{backgroundColor:theme.palette.primary.main,paddingTop:"3rem",paddingLeft:"3rem",paddingRight:"3rem",paddingBottom:"1.5rem",rowGap:"5rem",color:theme.palette.primary.light,justifyContent:"space-around"}}>
+    <Stack sx={{backgroundColor:theme.palette.primary.main,paddingTop:"3rem",paddingLeft:is700?"1rem":"3rem",paddingRight:is700?"1rem":"3rem",paddingBottom:"1.5rem",rowGap:"5rem",color:theme.palette.primary.light,justifyContent:"space-around"}}>
 
             {/* upper */}
-            <Stack flexDirection={'row'} justifyContent={'space-around'}>
+            <Stack flexDirection={'row'} rowGap={'1rem'} justifyContent={is700?"":'space-around'} flexWrap={'wrap'}>
 
-                <Stack rowGap={'1rem'}>
+                <Stack rowGap={'1rem'} padding={'1rem'}>
                     <Typography variant='h6' fontSize={'1.5rem'}>Exclusive</Typography>
                     <Typography variant='h6'>Subscribe</Typography>
                     <Typography sx={labelStyles}>Get 10% off your first order</Typography>
                     <TextField placeholder='Enter your email' sx={{border:'1px solid white',borderRadius:"6px"}} InputProps={{endAdornment:<IconButton><SendIcon sx={{color:theme.palette.primary.light}}/></IconButton>,style:{color:"whitesmoke"}}}/>
                 </Stack>
 
-                <Stack rowGap={'1rem'}>
+                <Stack rowGap={'1rem'} padding={'1rem'}>
                     <Typography variant='h6'>Support</Typography>
                     <Typography sx={labelStyles}>11th Main Street, Dhaka,  DH 1515, California.</Typography>
                     <Typography sx={labelStyles}>exclusive@gmail.com</Typography>
                     <Typography sx={labelStyles}>+88015-88888-9999</Typography>
                 </Stack>
 
-                <Stack rowGap={'1rem'}>
+                <Stack rowGap={'1rem'} padding={'1rem'}>
                     <Typography  variant='h6'>Account</Typography>
                     <Typography sx={labelStyles}>My Account</Typography>
                     <Typography sx={labelStyles}>Login / Register</Typography>
@@ -46,7 +47,7 @@ export const Footer = () => {
                     <Typography sx={labelStyles}>Shop</Typography>
                 </Stack>
 
-                <Stack rowGap={'1rem'}>
+                <Stack rowGap={'1rem'} padding={'1rem'}>
                     <Typography  variant='h6'>Quick Links</Typography>
                     <Typography sx={labelStyles}>Privacy Policy</Typography>
                     <Typography sx={labelStyles}>Terms Of Use</Typography>
@@ -54,7 +55,7 @@ export const Footer = () => {
                     <Typography sx={labelStyles}>Contact</Typography>
                 </Stack>
 
-                <Stack rowGap={'1rem'}>
+                <Stack rowGap={'1rem'} padding={'1rem'}>
                     <Typography  variant='h6'>Download App</Typography>
                     <Typography sx={{...labelStyles,color:"graytext",fontWeight:500}}>Save $3 with App New User Only</Typography>
                     <Stack flexDirection={'row'} columnGap={'.5rem'}>
