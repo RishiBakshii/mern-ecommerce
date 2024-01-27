8f449d08ae474058cee98eb6c7bb25f27e933e3a
rishibakshi <guitarguyrishi@gmail.com>
2024-01-27 20:11:24 +0530

    feat(CheckoutPage): Attach Footer component - Attach Footer component in 'CheckoutPage' for a visually balanced user interface

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Checkout } from '../features/checkout/components/Checkout'
+import {Footer} from '../features/footer/Footer'
 
 export const CheckoutPage = () => {
   return (
+    <>
     <Checkout/>
+    <Footer/>
+    </>
   )
 }
