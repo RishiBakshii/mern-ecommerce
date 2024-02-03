72a56d2db89bb7813a94f3852648cd801cfb4b95
rishibakshi <guitarguyrishi@gmail.com>
2024-02-03 23:33:57 +0530

    style(Navbar): Change brand name from 'ECOMMERCE' to 'MERN SHOP'

--- frontend/src/features/navigation/components/Navbar.jsx
+++ frontend/src/features/navigation/components/Navbar.jsx
@@ -57,8 +57,8 @@ export const Navbar=({isProductList=false})=> {
     <AppBar position="sticky" sx={{backgroundColor:"white",boxShadow:"none",color:"text.primary"}}>
         <Toolbar sx={{p:1,height:"4rem",display:"flex",justifyContent:"space-around"}}>
 
-          <Typography variant="h6" noWrap component="a" href="/" sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>
-            ECOMMERCE
+          <Typography variant="h6" noWrap component="a" href="/" sx={{ mr: 2, display: { xs: 'none', md: 'flex' },fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>
+            MERN SHOP
           </Typography>
 
 
