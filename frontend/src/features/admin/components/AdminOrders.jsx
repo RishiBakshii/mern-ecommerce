0baf87e89d3eb34f57afb751bed613ab61f6496c
rishibakshi <guitarguyrishi@gmail.com>
2024-02-05 14:04:30 +0530

    fix(Admin-Orders): Implement responsive layout for optimal viewing across devices

--- frontend/src/features/admin/components/AdminOrders.jsx
+++ frontend/src/features/admin/components/AdminOrders.jsx
@@ -8,11 +8,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Avatar, Button, Chip, FormControl, IconButton, InputLabel, MenuItem, Select, Stack, Typography, useTheme } from '@mui/material';
+import { Avatar, Button, Chip, FormControl, IconButton, InputLabel, MenuItem, Select, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
 import { useForm } from "react-hook-form"
 import { toast } from 'react-toastify';
+import {noOrdersAnimation} from '../../../assets/index'
+import Lottie from 'lottie-react'
 
 
 export const AdminOrders = () => {
@@ -22,6 +24,10 @@ export const AdminOrders = () => {
   const [editIndex,setEditIndex]=useState(-1)
   const orderUpdateStatus=useSelector(selectOrderUpdateStatus)
   const theme=useTheme()
+  const is1620=useMediaQuery(theme.breakpoints.down(1620))
+  const is1200=useMediaQuery(theme.breakpoints.down(1200))
+  const is820=useMediaQuery(theme.breakpoints.down(820))
+  const is480=useMediaQuery(theme.breakpoints.down(480))
 
   const {register,handleSubmit,formState: { errors },} = useForm()
 
@@ -81,103 +87,116 @@ export const AdminOrders = () => {
 
       <Stack mt={5} mb={3} component={'form'} noValidate onSubmit={handleSubmit(handleUpdateOrder)}>
 
-    <TableContainer component={Paper} elevation={2}>
-      <Table sx={{ minWidth: 1200 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Order</TableCell>
-            <TableCell align="left">Id</TableCell>
-            <TableCell align="left">Item</TableCell>
-            <TableCell align="right">Total Amount</TableCell>
-            <TableCell align="right">Shipping Address</TableCell>
-            <TableCell align="right">Payment Method</TableCell>
-            <TableCell align="right">Order Date</TableCell>
-            <TableCell align="right">Status</TableCell>
-            <TableCell align="right">Actions</TableCell>
-          </TableRow>
-        </TableHead>
+        {
+          orders.length?
+          <TableContainer sx={{width:is1620?"95vw":"auto",overflowX:'auto'}} component={Paper} elevation={2}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Order</TableCell>
+                  <TableCell align="left">Id</TableCell>
+                  <TableCell align="left">Item</TableCell>
+                  <TableCell align="right">Total Amount</TableCell>
+                  <TableCell align="right">Shipping Address</TableCell>
+                  <TableCell align="right">Payment Method</TableCell>
+                  <TableCell align="right">Order Date</TableCell>
+                  <TableCell align="right">Status</TableCell>
+                  <TableCell align="right">Actions</TableCell>
+                </TableRow>
+              </TableHead>
 
-        <TableBody>
+              <TableBody>
 
-          {
-          orders.length && orders.map((order,index) => (
-
-            <TableRow key={order._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-
-              <TableCell component="th" scope="row">{index}</TableCell>
-              <TableCell align="right">{order._id}</TableCell>
-              <TableCell align="right">
                 {
-                  order.item.map((product)=>(
-                    <Stack mt={2} flexDirection={'row'} alignItems={'center'} columnGap={2}>
-                      <Avatar src={product.product.thumbnail}></Avatar>
-                      <Typography>{product.product.title}</Typography>
-                    </Stack>
-                  ))
-                }
-              </TableCell>
-              <TableCell align="right">{order.total}</TableCell>
-              <TableCell align="right">
-                <Stack>
-                  <Typography>{order.address[0].street}</Typography>
-                  <Typography>{order.address[0].city}</Typography>
-                  <Typography>{order.address[0].state}</Typography>
-                  <Typography>{order.address[0].postalCode}</Typography>
-                </Stack>
-              </TableCell>
-              <TableCell align="right">{order.paymentMode}</TableCell>
-              <TableCell align="right">{new Date(order.createdAt).toDateString()}</TableCell>
+                orders.length && orders.map((order,index) => (
 
-              {/* order status */}
-              <TableCell align="right">
+                  <TableRow key={order._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
 
-                  {
-                    editIndex===index?(
-
-                  <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Update status</InputLabel>
-                    <Select
-                      defaultValue={order.status}
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      label="Update status"
-                      {...register('status',{required:'Status is required'})}
-                      >
-                      
+                    <TableCell component="th" scope="row">{index}</TableCell>
+                    <TableCell align="right">{order._id}</TableCell>
+                    <TableCell align="right">
                       {
-                        editOptions.map((option)=>(
-                          <MenuItem value={option}>{option}</MenuItem>
+                        order.item.map((product)=>(
+                          <Stack mt={2} flexDirection={'row'} alignItems={'center'} columnGap={2}>
+                            <Avatar src={product.product.thumbnail}></Avatar>
+                            <Typography>{product.product.title}</Typography>
+                          </Stack>
                         ))
                       }
-                    </Select>
-                  </FormControl>
-                  ):<Chip label={order.status} sx={getStatusColor(order.status)}/>
-                  }
-                
-              </TableCell>
+                    </TableCell>
+                    <TableCell align="right">{order.total}</TableCell>
+                    <TableCell align="right">
+                      <Stack>
+                        <Typography>{order.address[0].street}</Typography>
+                        <Typography>{order.address[0].city}</Typography>
+                        <Typography>{order.address[0].state}</Typography>
+                        <Typography>{order.address[0].postalCode}</Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell align="right">{order.paymentMode}</TableCell>
+                    <TableCell align="right">{new Date(order.createdAt).toDateString()}</TableCell>
 
-              {/* actions */}
-              <TableCell align="right">
+                    {/* order status */}
+                    <TableCell align="right">
 
-                {
-                  editIndex===index?(
-                    <Button>
+                        {
+                          editIndex===index?(
 
-                      <IconButton type='submit'><CheckCircleOutlinedIcon/></IconButton>
-                    </Button>
-                  )
-                  :
-                  <IconButton onClick={()=>setEditIndex(index)}><EditOutlinedIcon/></IconButton>
-                }
+                        <FormControl fullWidth>
+                          <InputLabel id="demo-simple-select-label">Update status</InputLabel>
+                          <Select
+                            defaultValue={order.status}
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Update status"
+                            {...register('status',{required:'Status is required'})}
+                            >
+                            
+                            {
+                              editOptions.map((option)=>(
+                                <MenuItem value={option}>{option}</MenuItem>
+                              ))
+                            }
+                          </Select>
+                        </FormControl>
+                        ):<Chip label={order.status} sx={getStatusColor(order.status)}/>
+                        }
+                      
+                    </TableCell>
 
-              </TableCell>
+                    {/* actions */}
+                    <TableCell align="right">
 
-            </TableRow>
-          ))}
+                      {
+                        editIndex===index?(
+                          <Button>
 
-        </TableBody>
-      </Table>
-    </TableContainer>
+                            <IconButton type='submit'><CheckCircleOutlinedIcon/></IconButton>
+                          </Button>
+                        )
+                        :
+                        <IconButton onClick={()=>setEditIndex(index)}><EditOutlinedIcon/></IconButton>
+                      }
+
+                    </TableCell>
+
+                  </TableRow>
+                ))}
+
+              </TableBody>
+            </Table>
+          </TableContainer>
+          :
+          <Stack width={is480?"auto":'30rem'} justifyContent={'center'}>
+
+            <Stack rowGap={'1rem'}>
+                <Lottie animationData={noOrdersAnimation}/>
+                <Typography textAlign={'center'} alignSelf={'center'} variant='h6' fontWeight={400}>There are no orders currently</Typography>
+            </Stack>
+              
+
+          </Stack>  
+        }
     
     </Stack>
     
