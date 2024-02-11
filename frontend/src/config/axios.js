611f6ee840e063667648f3e70687d64876041c5d
rishibakshi <guitarguyrishi@gmail.com>
2024-02-11 14:07:24 +0530

    config(axios): Update axios config to use environment variable for base URL
    - Replace hardcoded `http://localhost:8000` with `process.env.REACT_APP_BASE_URL`.
    - Improve flexibility and portability by storing the base URL securely in environment variable.

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const axiosi=axios.create({withCredentials:true,baseURL:"http://localhost:8000"})
+export const axiosi=axios.create({withCredentials:true,baseURL:process.env.REACT_APP_BASE_URL})
